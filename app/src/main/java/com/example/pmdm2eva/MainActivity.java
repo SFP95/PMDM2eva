8292fb1c4d4605f11f1835d202e773ea59d02014
alumno <dam.sonia.fernandez@gmail.com>
2023-02-24 12:51:54 +0100

    Ej 6-1

--- app/src/main/java/com/example/pmdm2eva/MainActivity.java
+++ app/src/main/java/com/example/pmdm2eva/MainActivity.java
@@ -6,29 +6,32 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    private EditText edt;
-    private Button b;
+    private RadioButton radioButton;
+    private RadioGroup radioGroup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        radioGroup= findViewById(R.id.radioGroup);
+        radioButton=findViewById(R.id.rbAgobiado);
+        radioButton.setChecked(true);
+        radioGroup.getCheckedRadioButtonId();
     }
 
-
-
-    public void onToast(View v) {
-        edt= findViewById(R.id.EdTexto);
-        String mensaje= edt.getText().toString();
-        Toast.makeText(this,mensaje,Toast.LENGTH_SHORT).show();
-
-    }
+    public void onComprobar(View view) {    }
 }
 
-/*EJ 6.0
+/* EJ 6.1 Radio grup que recuerda la opcion al cerrar al activity y volver a abrirse
+* */
+
+/*EJ 6.0 recoger texto de Edit text a un toast que se miestar al pulsar un botón
 *
 public class MainActivity extends AppCompatActivity {
     private EditText edt;
