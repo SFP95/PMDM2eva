55eb161a05dab91ef335c5dc0c54b2ee189843d2
alumno <dam.sonia.fernandez@gmail.com>
2023-02-24 12:01:11 +0100

    Ej 6-0

--- app/src/main/java/com/example/pmdm2eva/Ej6_0/MainActivity.java
+++ app/src/main/java/com/example/pmdm2eva/Ej6_0/MainActivity.java
@@ -1,12 +1,14 @@
-package com.example.pmdm2eva;
-
-import androidx.appcompat.app.AppCompatActivity;
+package com.example.pmdm2eva.Ej6_0;
 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.pmdm2eva.R;
 
 public class MainActivity extends AppCompatActivity {
     private EditText edt;
